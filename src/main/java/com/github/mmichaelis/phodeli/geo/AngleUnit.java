fb8624ab5e9b248379668c9c1ce26c8da12e21e6
Mark Michaelis <thragor@gmx.net>
2016-12-23 23:51:55 +0100

    Straighten Site Reporting

--- src/main/java/com/github/mmichaelis/phodeli/geo/AngleUnit.java
+++ src/main/java/com/github/mmichaelis/phodeli/geo/AngleUnit.java
@@ -1,42 +1,64 @@
 package com.github.mmichaelis.phodeli.geo;
 
-import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 
 /**
+ * Represents the unit of an angle, i. e. either degree or radians.
+ *
  * @author Mark Michaelis
  * @since 1.0.0
  */
 public enum AngleUnit {
+  /**
+   * Angle Unit: Degree.
+   *
+   * @since 1.0.0
+   */
   DEGREE("°") {
     @Override
-    public double convert(double sourceAngle, @NotNull AngleUnit sourceUnit) {
+    public double convert(final double sourceAngle, @NotNull final AngleUnit sourceUnit) {
       switch (sourceUnit) {
         case DEGREE:
           return sourceAngle;
         case RADIAN:
           return Math.toDegrees(sourceAngle);
+        default:
+          return super.convert(sourceAngle, sourceUnit);
       }
-      return super.convert(sourceAngle, sourceUnit);
     }
   },
+  /**
+   * Angle Unit: Radian.
+   *
+   * @since 1.0.0
+   */
   RADIAN("rad") {
     @Override
-    public double convert(double sourceAngle, @NotNull AngleUnit sourceUnit) {
+    public double convert(final double sourceAngle, @NotNull final AngleUnit sourceUnit) {
       switch (sourceUnit) {
         case DEGREE:
           return Math.toRadians(sourceAngle);
         case RADIAN:
           return sourceAngle;
+        default:
+          return super.convert(sourceAngle, sourceUnit);
       }
-      return super.convert(sourceAngle, sourceUnit);
     }
   };
 
-  @NonNls
-  private final @NotNull String symbol;
+  /**
+   * The symbol for the angle unit.
+   */
+  @NotNull
+  private final String symbol;
 
-  AngleUnit(@NonNls @NotNull String symbol) {
+  /**
+   * Constructor specifying angle unit symbol.
+   *
+   * @param symbol symbol to use
+   * @since 1.0.0
+   */
+  AngleUnit(@NotNull final String symbol) {
     this.symbol = symbol;
   }
 
@@ -49,13 +71,21 @@ public enum AngleUnit {
    * @param sourceAngle the angle in the given {@code sourceUnit}
    * @param sourceUnit  the unit of the {@code sourceAngle} argument
    * @return the converted angle in this unit
+   * @since 1.0.0
    */
-  public double convert(double sourceAngle, @NotNull AngleUnit sourceUnit) {
+  public double convert(final double sourceAngle, @NotNull final AngleUnit sourceUnit) {
     throw new AbstractMethodError(
       "Conversion from angle unit " + sourceUnit + " not supported yet.");
   }
 
-  public @NotNull String getSymbol() {
+  /**
+   * Return the symbol for the angle unit.
+   *
+   * @return symbol
+   * @since 1.0.0
+   */
+  @NotNull
+  public String getSymbol() {
     return symbol;
   }
 
---
+++ src/main/java/com/github/mmichaelis/phodeli/geo/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Classes to deal with geo-coordinates.
+ *
+ * @author Mark Michaelis
+ * @since 1.0.0
+ */
+package com.github.mmichaelis.phodeli.geo;
