a4a2249c26de279f40ccedcff509d85faf079bd6
Mark Michaelis <thragor@gmx.net>
2016-12-27 12:04:11 +0100

    AngleUnit: Introduce Normalization and Formatted Output

--- src/main/java/com/github/mmichaelis/phodeli/geo/AngleUnit.java
+++ src/main/java/com/github/mmichaelis/phodeli/geo/AngleUnit.java
@@ -1,6 +1,15 @@
 package com.github.mmichaelis.phodeli.geo;
 
+import static java.lang.Math.atan2;
+import static java.lang.Math.cos;
+import static java.lang.Math.sin;
+import static java.util.Locale.Category.FORMAT;
+
 import org.jetbrains.annotations.NotNull;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
 
 /**
  * Represents the unit of an angle, i. e. either degree or radians.
@@ -10,39 +19,45 @@ import org.jetbrains.annotations.NotNull;
  */
 public enum AngleUnit {
   /**
-   * Angle Unit: Degree.
+   * Angle Unit: Degree. Symbol: °.
    *
    * @since 1.0.0
    */
-  DEGREE("°") {
+  DEGREES("°", "#,##0.###'°'") {
     @Override
     public double convert(final double sourceAngle, @NotNull final AngleUnit sourceUnit) {
-      switch (sourceUnit) {
-        case DEGREE:
-          return sourceAngle;
-        case RADIAN:
-          return Math.toDegrees(sourceAngle);
-        default:
-          return super.convert(sourceAngle, sourceUnit);
-      }
+      return sourceUnit.toDegrees(sourceAngle);
+    }
+
+    @Override
+    public double toDegrees(final double sourceAngle) {
+      return sourceAngle;
+    }
+
+    @Override
+    public double toRadians(final double sourceAngle) {
+      return Math.toRadians(sourceAngle);
     }
   },
   /**
-   * Angle Unit: Radian.
+   * Angle Unit: Radian. Symbol: rad.
    *
    * @since 1.0.0
    */
-  RADIAN("rad") {
+  RADIANS("rad", "#,##0.#####' rad'") {
     @Override
     public double convert(final double sourceAngle, @NotNull final AngleUnit sourceUnit) {
-      switch (sourceUnit) {
-        case DEGREE:
-          return Math.toRadians(sourceAngle);
-        case RADIAN:
-          return sourceAngle;
-        default:
-          return super.convert(sourceAngle, sourceUnit);
-      }
+      return sourceUnit.toRadians(sourceAngle);
+    }
+
+    @Override
+    public double toDegrees(final double sourceAngle) {
+      return Math.toDegrees(sourceAngle);
+    }
+
+    @Override
+    public double toRadians(final double sourceAngle) {
+      return sourceAngle;
     }
   };
 
@@ -51,6 +66,11 @@ public enum AngleUnit {
    */
   @NotNull
   private final String symbol;
+  /**
+   * Format pattern to format angles.
+   */
+  @NotNull
+  private final String decimalFormatPattern;
 
   /**
    * Constructor specifying angle unit symbol.
@@ -58,24 +78,82 @@ public enum AngleUnit {
    * @param symbol symbol to use
    * @since 1.0.0
    */
-  AngleUnit(@NotNull final String symbol) {
+  AngleUnit(@NotNull final String symbol, @NotNull final String decimalFormatPattern) {
     this.symbol = symbol;
+    this.decimalFormatPattern = decimalFormatPattern;
   }
 
   /**
    * Converts the given angle in the given unit to this unit.
    *
    * <p>For example, to convert 0.5 rad to degree, use:
-   * {@code AngleUnit.DEGREE.convert(0.5D, TimeUnit.RADIAN)}
+   * {@code AngleUnit.DEGREES.convert(0.5D, TimeUnit.RADIANS)}
    *
    * @param sourceAngle the angle in the given {@code sourceUnit}
    * @param sourceUnit  the unit of the {@code sourceAngle} argument
    * @return the converted angle in this unit
    * @since 1.0.0
    */
-  public double convert(final double sourceAngle, @NotNull final AngleUnit sourceUnit) {
-    throw new AbstractMethodError(
-      "Conversion from angle unit " + sourceUnit + " not supported yet.");
+  public abstract double convert(final double sourceAngle, @NotNull final AngleUnit sourceUnit);
+
+  /**
+   * Equivalent to
+   * {@link #convert(double, AngleUnit) DEGREES.convert(duration, this)}.
+   *
+   * @param sourceAngle the angle
+   * @return the converted angle,
+   * @since 1.0.0
+   */
+  public abstract double toDegrees(double sourceAngle);
+
+  /**
+   * Equivalent to
+   * {@link #convert(double, AngleUnit) RADIANS.convert(duration, this)}.
+   *
+   * @param sourceAngle the angle
+   * @return the converted angle,
+   * @since 1.0.0
+   */
+  public abstract double toRadians(double sourceAngle);
+
+  /**
+   * Formats the angle given the current default locale.
+   *
+   * @param angle angle to format
+   * @return formatted string with unit symbol
+   * @since 1.0.0
+   */
+  @NotNull
+  public final String format(final double angle) {
+    return format(angle, Locale.getDefault(FORMAT));
+  }
+
+  /**
+   * Formats the angle given the given locale.
+   *
+   * @param angle angle to format
+   * @param loc   locale
+   * @return formatted string with unit symbol
+   * @since 1.0.0
+   */
+  @NotNull
+  public final String format(final double angle, @NotNull final Locale loc) {
+    NumberFormat nf = NumberFormat.getNumberInstance(loc);
+    DecimalFormat df = (DecimalFormat) nf;
+    df.applyPattern(getDecimalFormatPattern());
+    return df.format(angle);
+  }
+
+  /**
+   * Normalize the given angle for this angle unit.
+   *
+   * @param sourceAngle angle of this angle unit
+   * @return normalized angle
+   * @since 1.0.0
+   */
+  public final double normalized(final double sourceAngle) {
+    double asRadians = toRadians(sourceAngle);
+    return convert(atan2(sin(asRadians), cos(asRadians)), RADIANS);
   }
 
   /**
@@ -85,8 +163,19 @@ public enum AngleUnit {
    * @since 1.0.0
    */
   @NotNull
-  public String getSymbol() {
+  public final String getSymbol() {
     return symbol;
   }
 
+  /**
+   * Returns the decimal format pattern to be used for an angle with symbol as
+   * suffix.
+   *
+   * @return pattern
+   * @since 1.0.0
+   */
+  @NotNull
+  public final String getDecimalFormatPattern() {
+    return decimalFormatPattern;
+  }
 }
--- src/test/java/com/github/mmichaelis/phodeli/geo/AngleUnitTest.java
+++ src/test/java/com/github/mmichaelis/phodeli/geo/AngleUnitTest.java
@@ -1,18 +1,33 @@
 package com.github.mmichaelis.phodeli.geo;
 
-import static com.github.mmichaelis.phodeli.geo.AngleUnit.DEGREE;
-import static com.github.mmichaelis.phodeli.geo.AngleUnit.RADIAN;
+import static com.github.mmichaelis.phodeli.geo.AngleUnit.DEGREES;
+import static com.github.mmichaelis.phodeli.geo.AngleUnit.RADIANS;
+import static java.lang.Double.MAX_VALUE;
+import static java.lang.Math.PI;
+import static java.lang.Math.round;
 import static java.lang.Math.toDegrees;
 import static java.lang.Math.toRadians;
+import static java.lang.invoke.MethodHandles.lookup;
+import static java.util.Locale.Category.FORMAT;
+import static java.util.Locale.getDefault;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.withinPercentage;
+import static org.slf4j.LoggerFactory.getLogger;
 
+import org.assertj.core.api.SoftAssertions;
+import org.assertj.core.data.Offset;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestFactory;
+import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.api.function.ThrowingConsumer;
+import org.slf4j.Logger;
 
+import java.lang.reflect.Method;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Arrays;
 import java.util.Iterator;
+import java.util.Locale;
 import java.util.Random;
 import java.util.function.Function;
 import java.util.stream.Stream;
@@ -25,11 +40,16 @@ import java.util.stream.Stream;
  */
 class AngleUnitTest {
 
-  private static final int TEST_RUNS = 100;
+  private static final Logger LOG = getLogger(lookup().lookupClass());
+
+  private static final Offset<Double> TOLERANCE = Offset.offset(0.0001D);
+  private static final int TEST_RUNS = 20;
+  private static final Function<TestInfo, String> TEST_NAME =
+    input -> input.getTestMethod().map(Method::getName).orElse("unknown");
 
   @Test
   void radianHasExpectedSymbol() {
-    assertThat(RADIAN)
+    assertThat(RADIANS)
       .extracting(AngleUnit::getSymbol)
       .describedAs("Radian should specify 'rad' as symbol.")
       .containsExactly("rad");
@@ -37,68 +57,241 @@ class AngleUnitTest {
 
   @Test
   void degreeHasExpectedSymbol() {
-    assertThat(DEGREE)
+    assertThat(DEGREES)
       .extracting(AngleUnit::getSymbol)
       .describedAs("Degree should specify '°' as symbol.")
       .containsExactly("°");
   }
 
   @TestFactory
-  Stream<DynamicTest> degreeConvertingToItselfHasEqualValue() {
-    Iterator<Double> inputGenerator = new ValueGenerator(TEST_RUNS);
+  Stream<DynamicTest> degreeConvertingToItselfHasEqualValue(final TestInfo testInfo) {
+    Iterator<Double> inputGenerator = new ValueGenerator(TEST_RUNS, 720D);
     Function<Double, String>
       displayNameGenerator =
-      (input) -> "input: " + input + DEGREE.getSymbol();
-    ThrowingConsumer<Double> testExecutor = (input) ->
-      assertThat(DEGREE.convert(input, DEGREE))
-        .isEqualTo(input);
+      (input) -> TEST_NAME.apply(testInfo) + ", input: " + DEGREES.format(input);
+    ThrowingConsumer<Double> testExecutor = (input) -> {
+      SoftAssertions assertions = new SoftAssertions();
+      assertions.assertThat(DEGREES.convert(input, DEGREES)).isEqualTo(input);
+      assertions.assertThat(DEGREES.toDegrees(input)).isEqualTo(input);
+      assertions.assertAll();
+    };
     return DynamicTest.stream(inputGenerator, displayNameGenerator, testExecutor);
   }
 
   @TestFactory
-  Stream<DynamicTest> degreeCorrectlyConvertedToRadian() {
-    Iterator<Double> inputGenerator = new ValueGenerator(TEST_RUNS);
+  Stream<DynamicTest> degreeCorrectlyConvertedToRadian(final TestInfo testInfo) {
+    Iterator<Double> inputGenerator = new ValueGenerator(TEST_RUNS, 720D);
     Function<Double, String>
       displayNameGenerator =
-      (input) -> "input: " + input + DEGREE.getSymbol();
-    ThrowingConsumer<Double> testExecutor = (input) ->
-      assertThat(RADIAN.convert(input, DEGREE))
-        .isCloseTo(toRadians(input), withinPercentage(0.0001D));
+      (input) -> TEST_NAME.apply(testInfo) + ", input: " + DEGREES.format(input);
+    ThrowingConsumer<Double> testExecutor = (input) -> {
+      SoftAssertions assertions = new SoftAssertions();
+      assertions.assertThat(RADIANS.convert(input, DEGREES)).isCloseTo(toRadians(input), TOLERANCE);
+      assertions.assertThat(DEGREES.toRadians(input)).isCloseTo(toRadians(input), TOLERANCE);
+      assertions.assertAll();
+    };
     return DynamicTest.stream(inputGenerator, displayNameGenerator, testExecutor);
   }
 
   @TestFactory
-  Stream<DynamicTest> radianConvertingToItselfHasEqualValue() {
-    Iterator<Double> inputGenerator = new ValueGenerator(TEST_RUNS);
+  Stream<DynamicTest> radianConvertingToItselfHasEqualValue(final TestInfo testInfo) {
+    Iterator<Double> inputGenerator = new ValueGenerator(TEST_RUNS, PI * 2);
     Function<Double, String>
       displayNameGenerator =
-      (input) -> "input: " + input + RADIAN.getSymbol();
-    ThrowingConsumer<Double> testExecutor = (input) ->
-      assertThat(RADIAN.convert(input, RADIAN))
-        .isEqualTo(input);
+      (input) -> TEST_NAME.apply(testInfo) + ", input: " + RADIANS.format(input);
+    ThrowingConsumer<Double> testExecutor = (input) -> {
+      SoftAssertions assertions = new SoftAssertions();
+      assertions.assertThat(RADIANS.convert(input, RADIANS)).isEqualTo(input);
+      assertions.assertThat(RADIANS.toRadians(input)).isEqualTo(input);
+      assertions.assertAll();
+    };
     return DynamicTest.stream(inputGenerator, displayNameGenerator, testExecutor);
   }
 
   @TestFactory
-  Stream<DynamicTest> radianCorrectlyConvertedToDegree() {
-    Iterator<Double> inputGenerator = new ValueGenerator(TEST_RUNS);
+  Stream<DynamicTest> radianCorrectlyConvertedToDegree(final TestInfo testInfo) {
+    Iterator<Double> inputGenerator = new ValueGenerator(TEST_RUNS, PI * 2);
+    Function<Double, String> displayNameGenerator =
+      (input) -> TEST_NAME.apply(testInfo) + ", input: " + RADIANS.format(input);
+    ThrowingConsumer<Double> testExecutor = (input) -> {
+      SoftAssertions assertions = new SoftAssertions();
+      assertions.assertThat(DEGREES.convert(input, RADIANS)).isCloseTo(toDegrees(input), TOLERANCE);
+      assertions.assertThat(RADIANS.toDegrees(input)).isCloseTo(toDegrees(input), TOLERANCE);
+      assertions.assertAll();
+    };
+    return DynamicTest.stream(inputGenerator, displayNameGenerator, testExecutor);
+  }
+
+  @TestFactory
+  Stream<DynamicTest> degreesCorrectlyNormalized(final TestInfo testInfo) {
+    Iterator<Double> inputGenerator =
+      Arrays
+        .asList(-MAX_VALUE,
+                -361D, -360D, -359D,
+                -271D, -270D, -269D,
+                -181D, -180D, -179D,
+                -91D, -90D, -89D,
+                -1D, 0D, Double.MIN_VALUE, 1D,
+                89D, 90D, 91D,
+                269D, 270D, 271D,
+                359D, 360D, 361D,
+                MAX_VALUE)
+        .iterator();
+    Function<Double, String> displayNameGenerator =
+      (input) -> TEST_NAME.apply(testInfo) + ", input: " + DEGREES.format(input);
+    ThrowingConsumer<Double> testExecutor =
+      input -> {
+        SoftAssertions assertions = new SoftAssertions();
+        double normalized = DEGREES.normalized(input);
+        String inputAsString = DEGREES.format(input);
+        LOG.info("Degrees: {} normalized to {}", inputAsString, DEGREES.format(normalized));
+        assertions.assertThat(normalized)
+          .describedAs("Normalized value of %s must be greater than or equal to -180°",
+                       inputAsString)
+          .isGreaterThanOrEqualTo(-180D);
+        assertions.assertThat(normalized)
+          .describedAs("Normalized value of %s must be less than or equal to 180°", inputAsString)
+          .isLessThanOrEqualTo(180D);
+        if (input >= -180D && input <= 180D) {
+          assertions.assertThat(normalized)
+            .describedAs("Already normalized input %s should not change too much.", inputAsString)
+            .isCloseTo(input, TOLERANCE);
+        }
+        assertions.assertThat(normalized)
+          .describedAs("Normalizing %s again should provide a similar result.", inputAsString)
+          .isCloseTo(DEGREES.normalized(normalized), TOLERANCE);
+        assertions.assertAll();
+      };
+    return DynamicTest.stream(inputGenerator, displayNameGenerator, testExecutor);
+  }
+
+  @TestFactory
+  Stream<DynamicTest> radiansCorrectlyNormalized(final TestInfo testInfo) {
+    double offset = 0.01D;
+    Iterator<Double> inputGenerator =
+      Arrays
+        .asList(-MAX_VALUE,
+                -PI * 2 - offset, -PI * 2, -PI * 2 + offset,
+                -PI - offset, -PI, -PI + offset,
+                -PI / 2D - offset, -PI / 2D, -PI / 2D + offset,
+                -offset, 0D, Double.MIN_VALUE, offset,
+                PI / 2D - offset, PI / 2D, PI / 2D + offset,
+                PI - offset, PI, PI + offset,
+                PI * 2 - offset, PI * 2, PI * 2 + offset,
+                MAX_VALUE)
+        .iterator();
+    Function<Double, String> displayNameGenerator =
+      (input) -> TEST_NAME.apply(testInfo) + ", input: " + RADIANS.format(input);
+    ThrowingConsumer<Double> testExecutor =
+      input -> {
+        SoftAssertions assertions = new SoftAssertions();
+        double normalized = RADIANS.normalized(input);
+        String inputAsString = RADIANS.format(input);
+        LOG.info("Radians: {} normalized to {}", inputAsString, RADIANS.format(normalized));
+        assertions.assertThat(normalized)
+          .describedAs("Normalized value of %s must be greater than or equal to -π rad",
+                       inputAsString)
+          .isGreaterThanOrEqualTo(-PI);
+        assertions.assertThat(normalized)
+          .describedAs("Normalized value of %s must be less than or equal to π rad", inputAsString)
+          .isLessThanOrEqualTo(PI);
+        if (input >= -PI && input <= PI) {
+          assertions.assertThat(normalized)
+            .describedAs("Already normalized input %s should not change too much.", inputAsString)
+            .isCloseTo(input, TOLERANCE);
+        }
+        assertions.assertThat(normalized)
+          .describedAs("Normalizing %s again should provide a similar result.", inputAsString)
+          .isCloseTo(DEGREES.normalized(normalized), TOLERANCE);
+        assertions.assertAll();
+      };
+    return DynamicTest.stream(inputGenerator, displayNameGenerator, testExecutor);
+  }
+
+  @TestFactory
+  Stream<DynamicTest> radiansFormatDefaultsToDefaultLocale(final TestInfo testInfo) {
+    Iterator<Double> inputGenerator = new ValueGenerator(TEST_RUNS, PI * 2);
     Function<Double, String>
       displayNameGenerator =
-      (input) -> "input: " + input + RADIAN.getSymbol();
+      (input) -> TEST_NAME.apply(testInfo) + ", input: " + RADIANS.format(input);
     ThrowingConsumer<Double> testExecutor = (input) ->
-      assertThat(DEGREE.convert(input, RADIAN))
-        .isCloseTo(toDegrees(input), withinPercentage(0.0001D));
+      assertThat(RADIANS.format(input)).isEqualTo(RADIANS.format(input, getDefault(FORMAT)));
+    return DynamicTest.stream(inputGenerator, displayNameGenerator, testExecutor);
+  }
+
+  @TestFactory
+  Stream<DynamicTest> degreesFormatDefaultsToDefaultLocale(final TestInfo testInfo) {
+    Iterator<Double> inputGenerator = new ValueGenerator(TEST_RUNS, 720D);
+    Function<Double, String>
+      displayNameGenerator =
+      (input) -> TEST_NAME.apply(testInfo) + ", input: " + RADIANS.format(input);
+    ThrowingConsumer<Double> testExecutor = (input) ->
+      assertThat(DEGREES.format(input)).isEqualTo(DEGREES.format(input, getDefault(FORMAT)));
+    return DynamicTest.stream(inputGenerator, displayNameGenerator, testExecutor);
+  }
+
+  @TestFactory
+  Stream<DynamicTest> radiansFormattedCorrectly(final TestInfo testInfo) {
+    Locale testLocale = Locale.ROOT;
+    NumberFormat nf = NumberFormat.getNumberInstance(testLocale);
+    DecimalFormat df = (DecimalFormat) nf;
+    df.applyPattern("#,##0.##");
+
+    Iterator<Double> inputGenerator = new ValueGenerator(TEST_RUNS, PI * 2);
+    Function<Double, String> displayNameGenerator =
+      (input) -> TEST_NAME.apply(testInfo) + ", input: " + RADIANS.format(input);
+    ThrowingConsumer<Double> testExecutor = (input) -> {
+      String formatted = RADIANS.format(input, testLocale);
+      String expectedNumber = df.format(input);
+      // ignore possibly rounded last number
+      if (round(input) != input) {
+        expectedNumber = expectedNumber.substring(0, expectedNumber.length() - 1);
+      }
+      LOG.info("Radians: {} formatted to {}", input, formatted);
+      SoftAssertions assertions = new SoftAssertions();
+      assertions.assertThat(formatted).endsWith(RADIANS.getSymbol());
+      assertions.assertThat(formatted).startsWith(expectedNumber);
+      assertions.assertAll();
+    };
+    return DynamicTest.stream(inputGenerator, displayNameGenerator, testExecutor);
+  }
+
+  @TestFactory
+  Stream<DynamicTest> degreesFormattedCorrectly(final TestInfo testInfo) {
+    Locale testLocale = Locale.ROOT;
+    NumberFormat nf = NumberFormat.getNumberInstance(testLocale);
+    DecimalFormat df = (DecimalFormat) nf;
+    df.applyPattern("#,##0.##");
+
+    Iterator<Double> inputGenerator = new ValueGenerator(TEST_RUNS, PI * 2);
+    Function<Double, String> displayNameGenerator =
+      (input) -> TEST_NAME.apply(testInfo) + ", input: " + DEGREES.format(input);
+    ThrowingConsumer<Double> testExecutor = (input) -> {
+      String formatted = DEGREES.format(input, testLocale);
+      String expectedNumber = df.format(input);
+      // ignore possibly rounded last number
+      if (round(input) != input) {
+        expectedNumber = expectedNumber.substring(0, expectedNumber.length() - 1);
+      }
+      LOG.info("Degrees: {} formatted to {}", input, formatted);
+      SoftAssertions assertions = new SoftAssertions();
+      assertions.assertThat(formatted).endsWith(DEGREES.getSymbol());
+      assertions.assertThat(formatted).startsWith(expectedNumber);
+      assertions.assertAll();
+    };
     return DynamicTest.stream(inputGenerator, displayNameGenerator, testExecutor);
   }
 
   private static final class ValueGenerator implements Iterator<Double> {
 
     private final int runs;
-    Random random;
-    int current;
+    private final double maxValue;
+    private final Random random;
+    private int current;
 
-    private ValueGenerator(int runs) {
+    private ValueGenerator(final int runs, final double maxValue) {
       this.runs = runs;
+      this.maxValue = maxValue;
       random = new Random(0L);
       current = 0;
     }
@@ -111,7 +304,7 @@ class AngleUnitTest {
     @Override
     public Double next() {
       current++;
-      return random.nextDouble();
+      return random.nextDouble() * maxValue * 2 - maxValue;
     }
   }
 }
